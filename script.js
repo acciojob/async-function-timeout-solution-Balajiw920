9e0bac43673be552420d4570091740653310c7d8
Balajiw920 <wbalaji2021@gmail.com>
2023-04-05 04:15:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
- const text = document.getElementById("text");
-      const delay = document.getElementById("delay");
-      const btn = document.getElementById("btn");
-      const output = document.getElementById("output");
+const text = document.getElementById("text");
+const delay = document.getElementById("delay");
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
 
-      async function displayMessageWithDelay() {
-        const message = text.value;
-        const delayTime = Number(delay.value);
-        await new Promise((resolve) => setTimeout(resolve, delayTime));
-        output.innerText = message;
-      }
+//your code here
 
-      btn.addEventListener("click", displayMessageWithDelay);
+  const text = textInput.value;
+      const delayInSeconds = Number(delayInput.value);
+
+      outputDiv.innerText = "Waiting for " + delayInSeconds + " seconds...";
+
+      await new Promise(resolve => setTimeout(resolve, delayInSeconds * 1000));
+
+      outputDiv.innerText = "The message is: " + text;
+    }
+
+    const btn = document.getElementById("btn");
+    btn.addEventListener("click", showMessageAfterDelay);
+async function showMessage() {
+  const message = text.value;
+  const delayVal = delay.value;
+  await new Promise((resolve) => setTimeout(resolve, delayVal));
+  output.innerText = message;
+}
+
+btn.addEventListener("click", showMessage);
